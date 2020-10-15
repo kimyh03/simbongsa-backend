3cfc489f10d928d2c65fbbe3929f031d3d19d83d
Hoony <kimyh03@gmail.com>
2020-10-15 16:08:03 +0900

    Connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,42 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { PostModule } from './post/post.module';
-import { ApplicationModule } from './application/application.module';
-import { LikeModule } from './like/like.module';
-import { QuestionModule } from './question/question.module';
-import { AnswerModule } from './answer/answer.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { GraphQLModule } from '@nestjs/graphql';
+import { Post } from './post/post.entity';
+import { Application } from './application/application.entity';
+import { Like } from './like/like.entity';
+import { Question } from './question/question.entity';
+import { Answer } from './answer/answer.entity';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UserModule, PostModule, ApplicationModule, LikeModule, QuestionModule, AnswerModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.string().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User, Post, Application, Like, Question, Answer],
+      synchronize: true,
+      logging: true,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
