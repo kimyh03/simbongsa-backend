4a84e2331db429ea4a5a15d3961e9e6e7f62e13e
Hoony <kimyh03@gmail.com>
2020-11-30 16:59:33 +0900

    https

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
+
+const httpsOptions = {
+  key: readFileSync('./secrets/private.pem'),
+  cert: readFileSync('./secrets/private.crt'),
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
   await app.listen(3000);
 }
