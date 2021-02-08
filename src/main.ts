d48f3ce357cc44f03526907106244b8bad9018c8
김영훈 <hoony@gim-yeonghun-ui-MacBookAir.local>
2021-02-08 13:55:34 +0900

    chore : remove secret folder

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { readFileSync } from 'fs';
 
-const httpsOptions = {
-  key: readFileSync('./secrets/private.pem'),
-  cert: readFileSync('./secrets/private.crt'),
-};
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
 }
